4529d709de74f279e755a8e35a60f61def960703
Hyeon Kim <simnalamburt@gmail.com>
2016-04-01 13:06:58 +0900

    이리온 기능 부활

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import IRC from 'slate-irc'
 import tls from 'tls'
 // $FlowIssue: Use a local installed package
 import Kakao from 'kakao'
-import { sKey, duuid, table } from '../config.json'
+import { sKey, duuid, table, friends } from '../config.json'
 import { invert } from './utils.js'
 
 
@@ -49,6 +49,13 @@ kakao.on('message', ({ chat_id, user: { name, id: user_id }, message, type, atta
     console.log(`<@\x1b[33m${name}\x1b[0m \x1b[38;5;239m${user_id}\x1b[0m> ${message} \x1b[38;5;239m... ${chat_id}\x1b[0m`);
   }
 
+  // Friendly
+  if (message.includes('이리온') && friends.indexOf(user_id) >= 0) {
+    kakao.write(chat_id, '냐옹');
+  }
+
+
+  // Relay
   if (chat_id in kakao_irc) {
     const sanitized = name.split('').join('\x0f');
 
