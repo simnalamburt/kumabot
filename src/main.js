ac27efd62b6672cf7d7a582f44dea91913a4a1b2
Hyeon Kim <simnalamburt@gmail.com>
2016-04-06 05:38:06 +0900

    샾검색 요약

--- src/main.js
+++ src/main.js
@@ -43,39 +43,81 @@ irc.on('message', ({ from: name, to: channel, message }) => {
 });
 
 const kakao_irc = Object.freeze(invert(table));
-kakao.on('message', ({ chat_id, user: { name, id: user_id }, message, type, attachment }) => {
+kakao.on('message', data => {
+  const { chat_id, user: { name, id: user_id }, message } = data;
+
   // Debug purpose
   if (message.includes('김젼봇')) {
     console.log(`<@\x1b[33m${name}\x1b[0m \x1b[38;5;239m${user_id}\x1b[0m> ${message} \x1b[38;5;239m... ${chat_id}\x1b[0m`);
   }
 
-  // Friendly
+  // PING-PONG protocol
   if (message.includes('이리온') && friends.indexOf(user_id) >= 0) {
     kakao.write(chat_id, '냐옹');
   }
-
 
   // Relay
   if (chat_id in kakao_irc) {
     const sanitized = name.split('').join('\x0f');
 
     let lines: Array<string>;
-    switch (type) {
-    case 1: // Text
-      lines = message.replace(/\r/g, '').split('\n');
-      break;
-    case 2: // Photo
-      lines = [`(사진) ${attachment.url}`];
-      break;
-    default: // Unknown type
-      lines = [];
+    switch (data.type) {
+      case 1:  lines = message.replace(/\r/g, '').split('\n'); break; // Plain Text
+      case 2:  lines = [ `(이미지) ${data.attachment.url}` ]; break; // Image
+      case 23: lines = [ daum_search(data.attachment) ]; break; // Search
+      default: lines = [];
     }
 
-    lines.forEach(line => {
-      irc.send(kakao_irc[chat_id], `<${sanitized}> ${line}`);
-    });
+    lines.forEach(line => irc.send(kakao_irc[chat_id], `<${sanitized}> ${line}`));
+
+
   }
 });
+
+// 카카오 샾검색 결과를 한줄로 요약해주는 함수
+function daum_search(att: Object): string {
+  if (att.R.length === 1) {
+    const entry = att.R[0];
+
+    // 계산기
+    if (att.V === 'simple' && att.Q === '계산기') {
+      const result = entry.T.startsWith('=') ? entry.T.slice(1) : entry.T;
+      return `${entry.D} = ${result}`;
+    }
+
+    // 검색결과가 단일 이미지일경우
+    if (att.V === 'image') {
+      return `#${att.Q} (이미지) ${entry.I}`;
+    }
+
+    // 지도
+    if (att.V === 'list' && att.RH && att.RH.HI && att.RH.HI.L && att.RH.HI.TP === 'map') {
+      return `#${att.Q} (지도) ${att.RH.HI.L}`;
+    }
+
+    // 영화
+    if (att.V === 'movie' && entry.T && entry.DE && entry.L) {
+      return `#${entry.T} - ${entry.DE} ${entry.L}`;
+    }
+
+    // 날씨
+    const w = entry.MA;
+    if (att.V === 'weather' && w && w.length === 1 && w[0].T && w[0].TE) {
+      return `#${att.Q} - ${w[0].TE}°C ${w[0].T} ${att.L}`;
+    }
+
+    // 그 외에 검색결과가 하나일경우
+    if (att.V === 'list' && entry.D && entry.T && entry.L) {
+      const title = entry.T.length > 40 ?
+        entry.T.substring(0, 30) + '...' :
+        entry.T;
+
+      return `#${att.Q} - ${title} (${entry.D}) ${entry.L}`;
+    }
+  }
+
+  return `#${att.Q} ${att.L}`;
+}
 
 
 //
